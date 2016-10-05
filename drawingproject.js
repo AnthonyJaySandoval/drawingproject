ef12a497307cb736a020599673a3de1b1073a8cb
Anthony Sandoval <retrovirus546@gmail.com>
2016-10-05 14:03:48 -0700

    began to make mega man

--- drawingproject.js
+++ drawingproject.js
@@ -1,6 +1,15 @@
 var canvasDiv = document.getElementById("drawingArea");
 var canvas = canvasDiv.getContext('2d');
 
-canvas.fillStyle = "pink";
+canvas.fillStyle = "black";
+canvas.fillRect(0,0,1800,900);
 
-canvas.fillRect(80, 150, 100, 100);
+canvas.beginPath();
+canvas.fillStyle = "blue";
+canvas.arc(900,125,100,0,2*Math.PI,true);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "#FAEBD7";
+canvas.arc(910,140,80,0,2*Math.PI,true);
+canvas.fill();
